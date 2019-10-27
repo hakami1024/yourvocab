f234b73b1e05a6b88267b985ff450bc0135d1094
Amina Khakimova <hakami1024@gmail.com>
2019-10-27 23:03:38 +0100

    url fixed

--- yourvocab/urls.py
+++ yourvocab/urls.py
@@ -12,9 +12,7 @@ urlpatterns = [
     path('course/<int:course_id>/lesson/<int:lesson_id>/check', views.check),
     path('signup', views.signup),
     path('account_activation_sent', views.account_activation_sent),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-        views.activate, name='activate'),
+    path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
     path('accounts/', include('django.contrib.auth.urls')),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
-
