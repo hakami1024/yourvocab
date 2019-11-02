3ef9a988eb375cd6e7909a6d1b416798487e85e0
Amina Khakimova <hakami1024@gmail.com>
2019-11-02 22:22:56 +0100

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn YourVocab.wsgi
--- YourVocab/settings.py
+++ YourVocab/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'yourvocab/templates/yourvocab/../yourvocab/static')
+
+import django_heroku
+django_heroku.settings(locals())
