ce97efcdc5e2f3a19bfa6b4370d4c9ce227cf9bb
Amina Khakimova <hakami1024@gmail.com>
2019-11-21 16:55:18 +0100

    moving foreign code to vendor folder for github

--- yourvocab/static/js/vendor/bootstrap.bundle.js
+++ yourvocab/static/js/vendor/bootstrap.bundle.js
@@ -4,8 +4,8 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('yourvocab/static/js/vendor/jquery')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'yourvocab/static/js/vendor/jquery'], factory) :
   (global = global || self, factory(global.bootstrap = {}, global.jQuery));
 }(this, function (exports, $) { 'use strict';
 
